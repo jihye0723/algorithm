1e881db2ef0168ae078684b607a64ffc6ddad82a
jihye0723 <88600739+jihye0723@users.noreply.github.com>
2022-08-29 20:40:07 +0900

    Create 최지혜_JAVA_80ms.java

---
+++ 백준/10926_브론즈5/최지혜_JAVA_80ms.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_BaekJoon_10926_브론즈5 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(s+"??!");
+	}
+}
